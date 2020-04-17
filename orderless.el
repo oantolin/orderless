3b2741737b9e411362e6a8612487da3fe58132ab
Omar Antolín <omar.antolin@gmail.com>
2020-04-17 01:15:05 -0500

    Fix regression bug on no matches

--- orderless.el
+++ orderless.el
@@ -123,25 +123,26 @@ This function is part of the `orderless' completion style."
                               orderless-regexp-separator
                               t))
                (completions (all-completions prefix table pred)))
-          (when minibuffer-completing-file-name
-            (setq completions
-                  (completion-pcm--filename-try-filter completions)))
-          (nconc
-           (cl-loop for candidate in completions
-                    collect (orderless--highlight-matches
-                             completion-regexp-list
-                             candidate))
-           limit)))
+          (when completions
+            (when minibuffer-completing-file-name
+              (setq completions
+                    (completion-pcm--filename-try-filter completions)))
+            (nconc
+             (cl-loop for candidate in completions
+                      collect (orderless--highlight-matches
+                               completion-regexp-list
+                               candidate))
+             limit))))
     (invalid-regexp nil)))
 
 (defun orderless-try-completion (string table pred point &optional _metadata)
   "Complete STRING to unique matching entry in TABLE.
 This uses `orderless-all-completions' to find matches for STRING
-in TABLE among entries satisfying PRED (that function ignores
-POINT).  If there is only one match, it completes to that match.
-If there are no matches, it returns nil.  In any other case it
-\"completes\" STRING to itself.  This function is part of the
-`orderless' completion style."
+in TABLE among entries satisfying PRED.  If there is only one
+match, it completes to that match.  If there are no matches, it
+returns nil.  In any other case it \"completes\" STRING to
+itself, without moving POINT.
+This function is part of the `orderless' completion style."
   (let* ((limit (car (completion-boundaries string table pred "")))
          (prefix (substring string 0 limit))
          (all (orderless-all-completions string table pred point)))
