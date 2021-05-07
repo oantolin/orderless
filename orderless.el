8d187bd31e44e7e46fb1c3158c31c4a38e223618
Daniel Mendler <mail@daniel-mendler.de>
2021-05-07 14:55:47 +0200

    Only enable regexp and literal matching styles by default
    * Avoid performance issues with initialism when filtering long candidates (#39)
    * README: Update default matching styles
    * README: Document `orderless-define-completion-style`
    * README: Update `without-if-bang` such that it ignores "!"

--- orderless.el
+++ orderless.el
@@ -48,7 +48,7 @@
 ;; from strings to strings that map a component to a regexp to match
 ;; against.  The variable `orderless-matching-styles' lists the
 ;; matching styles to be used for components, by default it allows
-;; literal, regexp and initialism matching.
+;; literal and regexp matching.
 
 ;;; Code:
 
@@ -114,7 +114,7 @@ value means highlighting is skipped."
   :type '(choice boolean function))
 
 (defcustom orderless-matching-styles
-  '(orderless-literal orderless-regexp orderless-initialism)
+  '(orderless-literal orderless-regexp)
   "List of component matching styles.
 If this variable is nil, regexp matching is assumed.
 
