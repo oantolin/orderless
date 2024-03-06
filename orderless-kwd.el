fe2faa6992f845ca39a6757db473c8cffbabd73d
Daniel Mendler <mail@daniel-mendler.de>
2024-03-06 17:59:15 +0100

    Add orderless-kwd-read-only

--- orderless-kwd.el
+++ orderless-kwd.el
@@ -63,6 +63,7 @@
     (val ,#'orderless-kwd-value)
     (dif ,#'orderless-kwd-modified t)
     (key ,#'orderless-kwd-key t)
+    (ro  ,#'orderless-kwd-read-only t)
     (off ,#'orderless-kwd-off t)
     (on  ,#'orderless-kwd-on t))
   "Keyword dispatcher alist.
@@ -165,6 +166,12 @@ as a flag and does not require input."
   (lambda (str)
     (when-let ((buf (orderless-kwd--get-buffer str)))
       (buffer-modified-p buf))))
+
+(defun orderless-kwd-read-only (_)
+  "Match read-only buffers."
+  (lambda (str)
+    (when-let ((buf (orderless-kwd--get-buffer str)))
+      (buffer-local-value 'buffer-read-only buf))))
 
 (defun orderless-kwd-mode (pred regexp)
   "Match buffer mode or bookmark type against PRED and REGEXP."
