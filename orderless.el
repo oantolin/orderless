2b7a1688f24cc8ef5a3c3a6dab8e00d833bcda59
Daniel Mendler <mail@daniel-mendler.de>
2024-12-22 04:17:33 +0100

    Version 1.3

--- orderless.el
+++ orderless.el
@@ -5,7 +5,7 @@
 ;; Author: Omar Antolín Camarena <omar@matem.unam.mx>
 ;; Maintainer: Omar Antolín Camarena <omar@matem.unam.mx>, Daniel Mendler <mail@daniel-mendler.de>
 ;; Keywords: extensions
-;; Version: 1.2
+;; Version: 1.3
 ;; URL: https://github.com/oantolin/orderless
 ;; Package-Requires: ((emacs "27.1") (compat "30"))
 
